a9fc2df9560f5658ca3fa8e888e1478199e64525
sabahait <sabahaitskik@gmail.com>
2026-04-21 15:43:08 +0100

    update version final2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 3 - Déployée automatiquement final"
+    return "Version 4 - Déployée automatiquement fin "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
