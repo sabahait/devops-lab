392d06583b6dd38cf3c1f5f8bd9948332e0fc0d1
sabahait <sabahaitskik@gmail.com>
2026-04-23 14:45:06 +0100

    version day2-version8

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version final - Déployée automatiquement-day2-version 7"
+    return "Version final - Déployée automatiquement-day2-version 8"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
